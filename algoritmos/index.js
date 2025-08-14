8701306bd9dac24b54f8f64328010ea7b4587204
Erick Rost <erickrosts@gmail.com>
2025-08-13 21:54:11 -0300

    aula 1

---
+++ algoritmos/index.js
@@ -0,0 +1,18 @@
+/*
+idade = 15
+if (idade < 18){
+    console.log("menor de idade")
+}
+else (
+    console.log("maior de idade")
+)*/
+
+/*
+x = 10
+y = 20
+
+valor = x + y
+
+console.log(valor)
+*/
+
